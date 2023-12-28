1049b58c7c6458cd71e658ef4b11c560db952ac8
Ivo Havener <ivolucien@gmail.com>
2023-12-28 10:44:53 -0800

    test runner placeholder, minor test updates

--- src/tests/class-constructor.js
+++ src/tests/class-constructor.js
@@ -1,6 +1,7 @@
 import { expect } from 'chai'
 import Hindsight from '../index.js'
 
+// todo: consider adding either zora (minimal) or AVA (lightweight) test runner
 console.log('constructor tests...');
 
 console.log('depend on default constructor values');
@@ -18,18 +19,20 @@ expect(obj.module).to.equal(console);
 console.log('uses specified logger module');
 const FakeLogger = {
   fake: true,
+  getLogTableNames() { return ['info']; },
   info() { console.error('FakeLogger info() called') },
 }
 obj = new Hindsight(FakeLogger, {
+  // custom proxy
   stub: true,
-  getQueueNames() { return []; },
+  getLogTableNames() { return ['info']; },
 });
 expect(obj.name).to.equal('unknown')
 expect(obj.module).to.equal(FakeLogger);
 expect(obj.module).to.haveOwnProperty('fake');
 expect(obj.module.info()).to.not.throw;
 
-console.log('uses specified proxy');
+console.log('uses specified custom proxy');
 expect(obj.proxy).to.haveOwnProperty('stub');
 
 console.log('unknown logger without custom proxy fails');
@@ -37,5 +40,6 @@ try {
   obj = new Hindsight(FakeLogger);
   expect('to fail').to.equal('to never get here');
 } catch (err) {
+  console.log(err.message)
   expect(err).to.be.instanceOf(Error);
 }
--- src/tests/create-logger.js
+++ src/tests/create-logger.js
@@ -2,7 +2,7 @@ import { expect } from 'chai'
 import Hindsight from '../index.js'
 import util from 'util';
 
-console.log('createLogger tests...');
+console.log("\ncreateLogger tests...");
 
 // hindsight with all default options creates a proxied console
 let obj = new Hindsight();
@@ -10,4 +10,4 @@ expect(obj.name).to.equal('console');
 
 let logger = obj.createLogger();
 expect(util.types.isProxy(logger)).to.be.true;
-expect(logger)
+
