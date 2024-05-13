e17633dbe1b54a8d146649a0e1f13594ce3235f1
Ivo Havener <ivolucien@gmail.com>
2024-05-13 09:42:43 -0700

    fix example condition snippets use of this, and their tests

--- src/conditions.js
+++ src/conditions.js
@@ -4,41 +4,53 @@ const { trace } = getScopedLoggers('conditions')
 export default class ConditionFactory {
   // all created conditions are bound to the Hindsight instance on writeWhen assignment
 
-  // if passed an error log line, the returned `writeWhen` function will always write all buffered lines
-  static createDumpOnError (retroactiveLevelCutoff) {
-    return (metadata) => {
-      const isError = this.toInt(metadata.level) >= this.toInt('error')
+  static createDumpOnError (instance, retroactiveLevelCutoff) {
+    const condition = (metadata) => {
+      const isError = instance.toInt(metadata.level) >= instance.toInt('error')
       if (isError) {
         trace('dumpOnError triggered')
-        this.writeIf(retroactiveLevelCutoff) // write all of instance's buffered lines not below cutoff
+        instance.writeIf(retroactiveLevelCutoff) // write all of instance's buffered lines not below cutoff
       }
       return true // this writes the error line regardless of the overall cutoff level
     }
+    return condition.bind(instance) // Bind `condition` to `instance`
   }
 
-  // creates function returning true for every N lines or if allowed by its level, otherwise 'buffer'
-  static createOnEveryNth (countInterval) {
-    return (metadata) => {
-      this.lineCounter ? this.lineCounter += 1 : this.lineCounter = 1 // initialize or increment
-      const isNth = this.lineCounter % countInterval === 1
+  static createOnEveryNth (instance, countInterval) {
+    const condition = (metadata) => {
+      // initialize or increment a line counter for test purposes
+      instance.lineCounter = instance.lineCounter ? instance.lineCounter + 1 : 1
+      const isNth = instance.lineCounter % countInterval === 1
 
-      const belowCutoff = this.toInt(metadata.level) < this.toInt(this.writeWhen.level)
+      const belowCutoff = instance.toInt(metadata.level) < instance.toInt(instance.writeWhen.level)
 
       if (isNth) {
-        trace('onEveryNth triggered', { lineCount: this.lineCounter })
+        trace('onEveryNth triggered', { lineCount: instance.lineCounter })
       }
       return !belowCutoff || isNth
     }
+    return condition.bind(instance) // Bind `condition` to `instance`
   }
 
-  // creates function returning 'write' if function parameter returns a level not above the line level
-  // this example is a little redundant since the user can define a custom function for `writeWhen`
-  static createOnDynamicLevel (getDynamicLevel = (/* metadata, lineArgs */) => this.writeWhen.level) {
-    return (metadata, lineArgs) => {
+  /**
+   * Creates a condition that checks if the dynamic level returned is below the cutoff.
+   *
+   * @param {Object} instance - The Hindsight instance.
+   * @param {Function} getDynamicLevel - The user defined function that returns the dynamic level.
+   * @returns {Function} - The condition function.
+   *
+   * Returned condition function.
+   * @param {Object} metadata - The metadata object.
+   * @param {Array} lineArgs - The line arguments.
+   * @return {Boolean} Returns true if the dynamic level is at or above the cutoff, else false.
+   */
+  static createOnDynamicLevel (instance, getDynamicLevel) { // (metadata, lineArgs) => <code returning level string> */) {
+    const condition = (metadata, lineArgs) => {
       const effectiveLevel = getDynamicLevel(metadata, lineArgs) // return level string based on caller's logic
 
-      const belowCutoff = this.toInt(metadata.level) < this.toInt(effectiveLevel)
+      const belowCutoff = instance.toInt(metadata.level) < instance.toInt(effectiveLevel)
       return !belowCutoff
     }
+    return condition.bind(instance) // Bind `condition` to `instance`
   }
 }
--- src/tests/conditions.test.js
+++ src/tests/conditions.test.js
@@ -16,7 +16,7 @@ describe('ConditionFactory Tests', function () {
 
   describe('createDumpOnError', function () {
     it('should dump all logs on error', function () {
-      const dumpOnError = ConditionFactory.createDumpOnError.call(hindsight, 'debug')
+      const dumpOnError = ConditionFactory.createDumpOnError(hindsight, 'debug')
       hindsight.writeWhen.writeLineNow = dumpOnError
 
       // Simulate logging at different levels
@@ -36,7 +36,7 @@ describe('ConditionFactory Tests', function () {
 
   describe('createOnEveryNth', function () {
     it('should return true for every Nth log line', function () {
-      const onEveryNth = ConditionFactory.createOnEveryNth.call(hindsight, 2)
+      const onEveryNth = ConditionFactory.createOnEveryNth(hindsight, 2)
       hindsight.writeWhen = { writeLineNow: onEveryNth }
       hindsight.writeWhen.writeLineNow.bind(hindsight)
 
@@ -51,7 +51,7 @@ describe('ConditionFactory Tests', function () {
   describe('createOnDynamicLevel', function () {
     it('should return true if dynamic level allows for logging', function () {
       const dynamicLevelFunction = () => 'debug' // Dynamic level is 'debug'
-      const onDynamicLevel = ConditionFactory.createOnDynamicLevel.call(hindsight, dynamicLevelFunction)
+      const onDynamicLevel = ConditionFactory.createOnDynamicLevel(hindsight, dynamicLevelFunction)
       hindsight.writeWhen.writeLineNow = onDynamicLevel
 
       const metadata = { level: 'info' } // 'info' is above 'debug', so it should log
@@ -60,7 +60,7 @@ describe('ConditionFactory Tests', function () {
 
     it('should return false if dynamic level does not allow for logging', function () {
       const dynamicLevelFunction = () => 'warn' // Dynamic level is 'warn'
-      const onDynamicLevel = ConditionFactory.createOnDynamicLevel.call(hindsight, dynamicLevelFunction)
+      const onDynamicLevel = ConditionFactory.createOnDynamicLevel(hindsight, dynamicLevelFunction)
       hindsight.writeWhen.writeLineNow = onDynamicLevel
 
       const metadata = { level: 'info' } // 'info' is below 'warn', so it should not log
